2c263d4289bfc251d269e127236d0a00cc62f512
Dominic Ghizzoni <dominicghizzoni@ufl.edu>
2026-05-13 17:18:09 -0400

    Starter site

---
+++ src/components/CTA.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const CTA = () => {
+  return (
+    <section className="cta">
+        <p className='cta-text'>Contact me <br className="sm:block hidden" />
+        Here</p>
+        <Link to='/contact' className='btn'>
+            Contact
+        </Link>
+    </section>
+  )
+}
+
+export default CTA
